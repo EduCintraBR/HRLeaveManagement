9a5472bb3c8bbf223c437c273acae9ec1925f54f
Eduardo Cintra <educin15@gmail.com>
2023-10-19 21:03:21 -0300

    Adding AutoMapper and MediatR

---
+++ HRLeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using HRLeaveManagement.Domain;
+
+namespace HRLeaveManagement.Application.MappingProfiles
+{
+    public class LeaveTypeProfile : Profile
+    {
+        public LeaveTypeProfile()
+        {
+            CreateMap<LeaveTypeDto, LeaveType>().ReverseMap();
+        }
+    }
+}

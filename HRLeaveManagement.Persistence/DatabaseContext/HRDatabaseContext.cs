6aee91bcf58554ce5fa44345c18c81d82d5967af
Eduardo Cintra <educin15@hotmail.com>
2023-11-09 07:53:15 -0300

    Creation of Persistence app, and dbcontext class

---
+++ HRLeaveManagement.Persistence/DatabaseContext/HRDatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using HRLeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Persistence.DatabaseContext
+{
+    public class HRDatabaseContext : DbContext
+    {
+        public HRDatabaseContext(DbContextOptions<HRDatabaseContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+    }
+}
---
+++ HRLeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace HRLeaveManagement.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+    {
+
+
+        return services;
+    }
+}

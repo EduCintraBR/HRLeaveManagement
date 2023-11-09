02a61ee994ceb38cc869d410baceab750a68535a
Eduardo Cintra <educin15@gmail.com>
2023-11-09 16:48:53 -0300

    Adding DbContext and Configurations of ConnectionString

--- HRLeaveManagement.Persistence/PersistenceServiceRegistration.cs
+++ HRLeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using HRLeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace HRLeaveManagement.Persistence;
 
 public static class PersistenceServiceRegistration
 {
-    public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
     {
-
+        services.AddDbContext<HRDatabaseContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("HRDatabaseConnectionString"));
+        });
 
         return services;
     }

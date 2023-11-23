3c557fddc65c125d98b18918a9f672f292269172
Eduardo Cintra <educin15@gmail.com>
2023-11-23 14:14:45 -0300

    Implementing DeleteLeaveRequest Command

---
+++ HRLeaveManagement.Application/Features/LeaveRequest/Commands/DeleteLeaveRequest/DeleteLeaveRequestCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using HRLeaveManagement.Application.Contracts.Persistence;
+using HRLeaveManagement.Application.Exceptions;
+using MediatR;
+
+namespace HRLeaveManagement.Application.Features.LeaveRequest.Commands.DeleteLeaveRequest
+{
+    public class DeleteLeaveRequestCommandHandler : IRequestHandler<DeleteLeaveRequestCommand, Unit>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+
+        public DeleteLeaveRequestCommandHandler(IMapper mapper, ILeaveRequestRepository leaveRequestRepository)
+        {
+            this._mapper = mapper;
+            this._leaveRequestRepository = leaveRequestRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteLeaveRequestCommand request, CancellationToken cancellationToken)
+        {
+            var leaveRequest = await _leaveRequestRepository.GetByIdAsync(request.Id);
+
+            if (leaveRequest is null)
+                throw new NotFoundException(nameof(LeaveRequest), request.Id);
+
+            await _leaveRequestRepository.DeleteAsync(leaveRequest);
+
+            return Unit.Value;
+        }
+    }
+}

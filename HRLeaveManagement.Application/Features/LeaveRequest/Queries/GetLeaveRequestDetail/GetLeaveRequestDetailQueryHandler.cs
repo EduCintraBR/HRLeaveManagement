6ab4776d3deef11eafa352b4a25669ed0a5b4f6c
Eduardo Cintra <educin15@gmail.com>
2023-11-23 14:02:29 -0300

    Implementing Queries of LeaveRequest

---
+++ HRLeaveManagement.Application/Features/LeaveRequest/Queries/GetLeaveRequestDetail/GetLeaveRequestDetailQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using HRLeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HRLeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestDetail
+{
+    public class GetLeaveRequestDetailQueryHandler : IRequestHandler<GetLeaveRequestDetailQuery, LeaveRequestDetailsDto>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+
+        public GetLeaveRequestDetailQueryHandler(IMapper mapper, ILeaveRequestRepository leaveRequestRepository)
+        {
+            this._mapper = mapper;
+            this._leaveRequestRepository = leaveRequestRepository;
+        }
+
+        public async Task<LeaveRequestDetailsDto> Handle(GetLeaveRequestDetailQuery request, CancellationToken cancellationToken)
+        {
+            var leaveRequest = _mapper.Map<LeaveRequestDetailsDto>(await _leaveRequestRepository.GetLeaveRequestWithDetails(request.Id));
+
+            // Add Employee details as needed
+
+            return leaveRequest;
+        }
+    }
+}
---
+++ HRLeaveManagement.Application/Features/LeaveRequest/Queries/GetLeaveRequestList/GetLeaveRequestListQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HRLeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HRLeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestList
+{
+    public class GetLeaveRequestListQueryHandler : IRequestHandler<GetLeaveRequestListQuery, List<LeaveRequestListDto>>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+
+        public GetLeaveRequestListQueryHandler(IMapper mapper, ILeaveRequestRepository leaveRequestRepository)
+        {
+            this._mapper = mapper;
+            this._leaveRequestRepository = leaveRequestRepository;
+        }
+
+        public async Task<List<LeaveRequestListDto>> Handle(GetLeaveRequestListQuery request, CancellationToken cancellationToken)
+        {
+            // Check if it is logged in employee
+
+            var leaveRequests = await _leaveRequestRepository.GetLeaveRequestsWithDetails();
+            var requests = _mapper.Map<List<LeaveRequestListDto>>(leaveRequests);
+
+            // Fill requests with employee information
+
+            return requests;
+        }
+    }
+}

a91ed1d5d507fddc828d0d9f9b431870349d2b49
Eduardo Cintra <educin15@gmail.com>
2023-11-24 08:27:24 -0300

    Creating an UnitTest project to our App

---
+++ HRLeaveManagement.Application.UnitTestes/Mocks/MockLeaveTypeRepository.cs
@@ -0,0 +1,57 @@
+﻿using HRLeaveManagement.Application.Contracts.Persistence;
+using HRLeaveManagement.Domain;
+using Moq;
+
+namespace HRLeaveManagement.Application.UnitTestes.Mocks
+{
+    public class MockLeaveTypeRepository
+    {
+        public static Mock<ILeaveTypeRepository> GetMockLeaveTypeRepository()
+        {
+            var leaveTypes = new List<LeaveType>
+            {
+                new LeaveType
+                {
+                    Id = 1,
+                    DefaultDays = 10,
+                    Name = "Test Vacation"
+                },
+                new LeaveType
+                {
+                    Id = 2,
+                    DefaultDays = 15,
+                    Name = "Test Sick"
+                },
+                new LeaveType
+                {
+                    Id = 3,
+                    DefaultDays = 15,
+                    Name = "Test Maternity"
+                }
+            };
+
+            var mockRepo = new Mock<ILeaveTypeRepository>();
+
+            mockRepo.Setup(s => s.GetAllAsync()).ReturnsAsync(leaveTypes);
+
+            mockRepo.Setup(s => s.CreateAsync(It.IsAny<LeaveType>()))
+                .Returns((LeaveType leaveType) =>
+                {
+                    leaveTypes.Add(leaveType);
+                    return Task.CompletedTask;
+                });
+
+            mockRepo.Setup(s => s.UpdateAsync(It.IsAny<LeaveType>()))
+                .Returns((LeaveType leaveType) =>
+                {
+                    var item = leaveTypes.FirstOrDefault(p => p.Id == leaveType.Id);
+                    leaveTypes.Remove(item);
+                    leaveTypes.Add(leaveType);
+
+                    return Task.CompletedTask;
+                });
+
+            return mockRepo;
+        }
+    }
+}
